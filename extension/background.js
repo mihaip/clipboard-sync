d557d2160014cb955f70ad3218e469a0ad6cee33
Mihai Parparita <mihaip@chromium.org>
2012-08-14 22:04:07 -0700

    Initial barebones clipboard sync extension.

---
+++ extension/background.js
@@ -0,0 +1,42 @@
+var storage = chrome.storage.sync;
+
+var CLIPBOARD_DATA_KEY = 'clipboard-data';
+
+chrome.browserAction.onClicked.addListener(function() {
+  var bufferNode = document.getElementById('buffer');
+  bufferNode.value = '';
+  bufferNode.focus();
+  bufferNode.selectionStart = bufferNode.selectionEnd = 0;
+  if (!document.execCommand('paste')) {
+    // TODO(mihaip): error handling
+    alert('Couldn\'t paste into buffer');
+    return;
+  }
+  var clipboardData = bufferNode.value + '1';
+  var data = {};
+  data[CLIPBOARD_DATA_KEY] = clipboardData;
+  storage.set(data);
+});
+
+chrome.storage.onChanged.addListener(function(changes, storageNamespace) {
+  if (storageNamespace != 'sync') {
+    return;
+  }
+
+  if (!CLIPBOARD_DATA_KEY in changes) {
+    return;
+  }
+
+  var clipboardData = changes[CLIPBOARD_DATA_KEY].newValue;
+
+  var bufferNode = document.getElementById('buffer');
+  bufferNode.value = clipboardData;
+  bufferNode.focus();
+  bufferNode.selectionStart = 0;
+  bufferNode.selectionEnd = clipboardData.length;
+  if (!document.execCommand('copy')) {
+    // TODO(mihaip): error handling
+    alert('Couldn\'t copy from buffer');
+    return;
+  }
+});
